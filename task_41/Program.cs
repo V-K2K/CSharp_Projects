5b895a6808b7fb739f5b854f9d32c490cf5a4abe
V <vmoskaltsov@gmail.com>
2023-07-10 07:56:02 -0600

    Home Work #6 (41 and 43)

---
+++ task_41/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.WriteLine("Enter the quantity: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[M];
+for (int i = 0; i < M; i++)
+{
+    Console.WriteLine("Enter a number: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int count = 0;
+for (int i = 0; i < M; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"The count of positive numbers is: {count}");
